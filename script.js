9e2aed200cbace08bcbefe2a8867a81d317b14db
lilithkarapetyan <lilith.karapetyann@gmail.com>
2023-11-13 19:29:50 +0400

    added stroke, rect, text,textsize,etc

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-debugger;
-console.log(areaCircle(2));
+function setup() {
+    createCanvas(500, 500);
+    background(100, 200, 100);
+}
+// console.log({...window});
+function draw() {
+    rect(0, 0, 100, 100);
+    textSize(70);
+    stroke(255, 0, 0);
+    text("Hello", width / 2, height / 2)
+}
