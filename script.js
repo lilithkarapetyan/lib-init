5fa5142c6a12f38e67e76e2770d784fd393185ce
lilithkarapetyan <lilith.karapetyann@gmail.com>
2023-11-15 17:09:46 +0400

    more functions

--- script.js
+++ script.js
@@ -7,5 +7,6 @@ function draw() {
     rect(0, 0, 100, 100);
     textSize(70);
     stroke(255, 0, 0);
-    text("Hello", width / 2, height / 2)
+    text("Hello", width / 2, height / 2);
+    ellipse(width / 2, height / 2, 100, 100)
 }
