8be156a1ca72ddb087c665807a8518cdad57c19f
lilithkarapetyan <lilith.karapetyann@gmail.com>
2023-11-15 17:21:38 +0400

    added image

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+let img; 
+
+function preload() {
+    img = loadImage("https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcQHmko3ML9W6M3PPzNlD9yXfvvPAlFmZCofed3qQ-ens3_aDfrohWPrPveo042VMHWn");
+}
+
 function setup() {
     createCanvas(500, 500);
-    background(100, 200, 100);
+    background(100, 200, 100);  
 }
 
 function draw() {
@@ -8,5 +14,6 @@ function draw() {
     textSize(70);
     stroke(255, 0, 0);
     text("Hello", width / 2, height / 2);
-    ellipse(width / 2, height / 2, 100, 100)
+    ellipse(width / 2, height / 2, 100, 100);
+    image(img, 0, 0, width, height);
 }
