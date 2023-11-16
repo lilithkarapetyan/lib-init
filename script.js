a0ebd176d2a33d6080910b0e07a40dc99f3c6f68
lilithkarapetyan <lilith.karapetyann@gmail.com>
2023-11-16 18:05:26 +0400

    more functions

--- script.js
+++ script.js
@@ -1,19 +1,127 @@
-let img; 
-
-function preload() {
-    img = loadImage("https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcQHmko3ML9W6M3PPzNlD9yXfvvPAlFmZCofed3qQ-ens3_aDfrohWPrPveo042VMHWn");
-}
+const words = getWords();
+const visibleWords = [];
+let currentWord = null;
+let currentIndex = -1;
+let round = 1;
+let timeoutId;
+let isFinished = false;
+let backgroundMusic;
+let startMusic = true;
+let img = null;
 
 function setup() {
-    createCanvas(500, 500);
-    background(100, 200, 100);  
+    createCanvas(window.innerWidth, window.innerHeight);
+    // backgroundMusic = loadSound("./assets/sounds/background_music.mp3")
+    // play();
+    img = loadImage("https://www.imgonline.com.ua/examples/random-pixels-background-big.jpg");
+}
+
+function mousePressed() {
+    if(startMusic) {
+        // backgroundMusic.play();
+        startMusic = false;
+    }
 }
 
 function draw() {
-    rect(0, 0, 100, 100);
-    textSize(70);
-    stroke(255, 0, 0);
-    text("Hello", width / 2, height / 2);
-    ellipse(width / 2, height / 2, 100, 100);
-    image(img, 0, 0, width, height);
+    background(img, 0, 0);
+    fill(WORD_COLOR);
+    for (let word of visibleWords) {
+        const visibleText = word === currentWord ? word.text.slice(currentIndex): word.text;
+        text(visibleText, word.x, Math.floor(word.y));
+        word.y += WORD_SPEED;
+    }
+
+    const [firstWord] = visibleWords;
+    if (visibleWords.length && firstWord.y > height) {
+        lose();
+    } else if(!words.length && round >= MAX_ROUNDS && !visibleWords.length) {
+        win();
+    }
+
+}
+
+function keyTyped() {
+    checkLetter(key);
+}
+
+function checkLetter(letter) {
+    if (!currentWord) {
+        selectWord(letter);
+    } else {
+        checkWordNextLetter(letter);
+    }
+
+    if (currentWord.text.length === currentIndex) {
+        visibleWords.splice(visibleWords.indexOf(currentWord), 1);
+        currentWord = null;
+        currentIndex = -1;
+    }
+}
+
+function selectWord(letter) {
+    const word = visibleWords.find(item => {
+        const [firstLetter] = item.text;
+        return firstLetter === letter;
+    });
+    currentWord = word;
+    currentIndex = 1;
+}
+
+function checkWordNextLetter(letter) {
+    const expectedNextLetter = currentWord.text.charAt(currentIndex);
+    if (letter === expectedNextLetter) {
+        currentIndex++;
+    }
+}
+
+function getWords() {
+    const words = WORDS.filter(word => word.length <= MAX_LETTER_COUNT)
+
+    return words.map((word) => ({
+        text: word,
+        x: Math.random() * MAX_WORD_X,
+        y: 0,
+    }));
+}
+
+function play() {
+    let spawningInterval = INITIAL_SPAWNING_INTERVAL;
+    function loop() {
+        timeoutId = setTimeout(() => {
+            addWord();
+            if(spawningInterval > MIN_SPAWNING_INTERVAL) {
+                spawningInterval -= SPAWNING_INTERVAL_ACCELERATION;
+            }
+            if(!isFinished) {
+                timeoutId = setTimeout(loop, spawningInterval);
+            }
+        }, spawningInterval);
+    }
+    loop();
+}
+
+function addWord() {
+    if(!words.length) {
+        if(round < MAX_ROUNDS) {
+            words.push(...getWords());
+            round++;
+        }
+    } else {
+        visibleWords.push(words.pop());
+    }
+}
+function finish() {
+    isFinished = true;
+    clearTimeout(timeoutId);
+    noLoop();
+}
+function win() {
+    finish();
+    alert("You won");
+}
+
+function lose() {
+    finish();
+    alert("You lost");
 }
